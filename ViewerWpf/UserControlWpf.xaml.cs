5f032e09fbb4f2fe1ac15396743561b669229f5c
Ivan Fuligni <i.fuligni@mindeskvr.com>
2021-02-17 10:21:27 +0100

    Build with NET5 without MapControl

--- ViewerWpf/UserControlWpf.xaml.cs
+++ ViewerWpf/UserControlWpf.xaml.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
-using Microsoft.Toolkit.Wpf.UI.Controls;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 
 namespace ViewerWpf
 {
@@ -13,16 +10,6 @@ namespace ViewerWpf
         public UserControlWpf()
         {
             InitializeComponent();
-        }
-
-        private async void MapControl_Loaded(object sender, RoutedEventArgs e)
-        {
-            // Specify a known location.
-            BasicGeoposition cityPosition = new BasicGeoposition() { Latitude = 47.604, Longitude = -122.329 };
-            var cityCenter = new Geopoint(cityPosition);
-
-            // Set the map location.
-            await (sender as MapControl).TrySetViewAsync(cityCenter, 12);
         }
     }
 }
